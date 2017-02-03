e7c5f93d352323668dbd5168fc67f0be2c871447
Luis Díaz Más <piponazo@gmail.com>
2017-02-03 20:28:52 +0100

    test_package working on Windows

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -10,28 +10,26 @@ class LiblasTestConan(ConanFile):
     generators = 'cmake'
 
     def imports(self):
-        if self.settings.os == 'Windows':
-            self.copy('*.dll', src='bin', dst=os.sep.join(['.', 'bin', '%s' % self.settings.build_type]))
-        elif self.settings.os == 'Macos':
-            self.copy('*.dylib', src='dylibs', dst=os.sep.join(['.', 'bin']))
-        else:
-            self.copy('*.so*', src='lib', dst=os.sep.join(['.', 'bin']))
+       self.copy('*.dll', src='x64/vc12/bin/', dst=os.sep.join(['.', 'bin', '%s' % self.settings.build_type]))
+       self.copy('*.dll', src='bin', dst=os.sep.join(['.', 'bin', '%s' % self.settings.build_type]))
+       self.copy('*.dylib', src='lib', dst='bin')
+       self.copy('*.so*',   src='lib', dst='bin')
 
     def build(self):
-        cmake = CMake(self.settings)
+       cmake = CMake(self.settings)
 
-        # TODO : Remove CMAKE_MODULE_PATH when Aruco CMake code improves
-        cmake_options = ['CMAKE_BUILD_TYPE=%s' % self.settings.build_type,
-                         'CMAKE_MODULE_PATH="%s"' % self.deps_cpp_info['ArUco'].rootpath +
-                         '/lib/cmake']
-        options = '-D' + ' -D'.join(cmake_options)
+       # TODO : Remove CMAKE_MODULE_PATH when Aruco CMake code improves
+       cmake_options = ['CMAKE_BUILD_TYPE=%s' % self.settings.build_type,
+                        'CMAKE_MODULE_PATH="%s"' % self.deps_cpp_info['ArUco'].rootpath +
+                        '/lib/cmake']
+       options = '-D' + ' -D'.join(cmake_options)
 
-        self.run('cmake "%s" %s %s' % (self.conanfile_directory, cmake.command_line, options))
-        self.run('cmake --build . %s' % cmake.build_config)
+       self.run('cmake "%s" %s %s' % (self.conanfile_directory, cmake.command_line, options))
+       self.run('cmake --build . %s' % cmake.build_config)
 
     def test(self):
-        os.putenv('DYLD_LIBRARY_PATH', os.sep.join(['.', 'bin']))
-        if self.settings.os == 'Windows':
-            self.run(os.sep.join(['.', 'bin', '%s' % self.settings.build_type, 'test']))
-        else:
-            self.run(os.sep.join(['.', 'bin', 'test']))
+       os.putenv('DYLD_LIBRARY_PATH', os.sep.join(['.', 'bin']))
+       if self.settings.os == 'Windows':
+           self.run(os.sep.join(['.', 'bin', '%s' % self.settings.build_type, 'test']))
+       else:
+           self.run(os.sep.join(['.', 'bin', 'test']))
